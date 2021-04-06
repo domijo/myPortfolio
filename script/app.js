89bea03ba9e9902c024e069419c9b0f4282b0a56
Domien Nguetsop <72477341+domijo@users.noreply.github.com>
2021-04-06 18:13:29 +0200

    4epartie

---
+++ script/app.js
@@ -0,0 +1,22 @@
+$(function(){
+    'use strict';
+    $('#portfolio ul.portfolio-filter li').on('click', function(){
+        // var linkValue = $(this).attr()
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+});
+
+$(function(){
+    'use strict';
+    $('#portfolio ul.portfolio-filter li:first-of-type').on('click', function(){
+        $('#portfolio .portfolio-items .item').hide(0).fadeIn(0);
+    });
+});
+
+$(function(){
+    'use strict';
+    $('#portfolio ul.portfolio-filter li:not(:first-of-type)').on('click',function(){
+        $('#portfolio .portfolio-items .item').hide(0);
+        $('.' + $(this).data('value')).show(0);
+    });
+});
